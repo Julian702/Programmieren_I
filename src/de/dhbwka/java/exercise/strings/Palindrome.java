e499f01d546bc4ff3625cc09a5f57ab74a677e4b
Julian <julian70202@gmail.com>
2022-02-21 16:48:16 +0100

    added strings/Palindrome.java

---
+++ src/de/dhbwka/java/exercise/strings/Palindrome.java
@@ -0,0 +1,20 @@
+package de.dhbwka.java.exercise.strings;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    
+    public static void main(String[] args) {
+        System.out.println("Bitte Wort eingeben:");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        StringBuilder strb = new StringBuilder(s);
+        String sr = strb.reverse().toString();
+        System.out.println("Umgekehrt: " + sr);
+        if(s.equalsIgnoreCase(sr)){
+            System.out.println(s + " ist ein Palindrom!");
+        }else{
+            System.out.println(s + " ist kein Palindrom!");
+        }
+    }
+}
